2b86b5912cdac87ffba001d30b9b5e17521f83f6
carlosmperilla <105413945+carlosmperilla@users.noreply.github.com>
2022-07-24 10:08:49 -0300

    Fixed bug in environment reassignment.

--- budget_system/settings/LanguagesText.py
+++ budget_system/settings/LanguagesText.py
@@ -19,10 +19,6 @@ from .Lang import LANG
 #Get the current path
 current_path = pathlib.Path(__file__).parent.absolute()
 
-#This read the language configuration text file
-config = configparser.ConfigParser()
-config.read(os.path.join(current_path, f"languages/{LANG}.ini"), encoding="utf-8")
-
 def generate_lang_texts(lang:str, sections:Dict[str, Dict[str, str]]) -> configparser.ConfigParser:
     """This generates a {LANG}.ini file with the necessary texts to work in languages other than the default ones.
      Or to overwrite message texts for the current setting.
@@ -56,30 +52,49 @@ class Language:
     """Class that contains the basic texts of each configuration,
     based on the default language in the app.
     """
-
+    
     class Month:
-        message_MonthNotValid = config["Month"]["message_MonthNotValid"]
-        message_MonthEmpty = config["Month"]["message_MonthEmpty"]
+        def __init__(self):
+            self.message_MonthNotValid = config["Month"]["message_MonthNotValid"]
+            self.message_MonthEmpty = config["Month"]["message_MonthEmpty"]
 
     class ShowData:
-        message_TotalSpent = config["ShowData"]["message_TotalSpent"]
-        message_YearSpent = config["ShowData"]["message_YearSpent"]
-        message_MonthSpent = config["ShowData"]["message_MonthSpent"]
-        message_DateSpent = config["ShowData"]["message_DateSpent"]
-        message_LeastExpensive = config["ShowData"]["message_LeastExpensive"]
-        message_MostExpensive = config["ShowData"]["message_MostExpensive"]
+        def __init__(self):
+            self.message_TotalSpent = config["ShowData"]["message_TotalSpent"]
+            self.message_YearSpent = config["ShowData"]["message_YearSpent"]
+            self.message_MonthSpent = config["ShowData"]["message_MonthSpent"]
+            self.message_DateSpent = config["ShowData"]["message_DateSpent"]
+            self.message_LeastExpensive = config["ShowData"]["message_LeastExpensive"]
+            self.message_MostExpensive = config["ShowData"]["message_MostExpensive"]
     
     class BudgetSystem:
-        message_FolderTreeDoesntExist = config["BudgetSystem"]["message_FolderTreeDoesntExist"]
+        def __init__(self):
+            self.message_FolderTreeDoesntExist = config["BudgetSystem"]["message_FolderTreeDoesntExist"]
         
     class Settings:
-        message_InvalidConfigFile = config["Settings"]["message_InvalidConfigFile"]
-        message_UnallocatedConfigFile = config["Settings"]["message_UnallocatedConfigFile"]
+        def __init__(self):
+            self.message_InvalidConfigFile = config["Settings"]["message_InvalidConfigFile"]
+            self.message_UnallocatedConfigFile = config["Settings"]["message_UnallocatedConfigFile"]
+
+def init_language_config() -> None:
+    """
+       This read the language configuration text file if it was assigned.
+    """
+
+    global config
+    config = configparser.ConfigParser()
+    LANG_ = LANG
+    if os.environ.get('CONFIG_BUDGET'):
+        config_budget = configparser.ConfigParser()
+        config_budget.read(os.environ["CONFIG_BUDGET"], encoding="utf-8")
+        LANG_ = config_budget["DEFAULT"]["lang"]
+    config.read(os.path.join(current_path, f"languages/{LANG_}.ini"), encoding="utf-8")
 
 def get_month_text() -> Tuple[str]:
     """This gets the text for the Month class"""
+    init_language_config()
 
-    lang_month_messages = Language.Month
+    lang_month_messages = Language.Month()
 
     return (
         lang_month_messages.message_MonthNotValid,
@@ -88,8 +103,9 @@ def get_month_text() -> Tuple[str]:
 
 def get_show_data_text() -> Tuple[str]:
     """This gets the text for the show_data"""
-
-    lang_show_data_messages = Language.ShowData
+    init_language_config()
+    
+    lang_show_data_messages = Language.ShowData()
 
     return (
         lang_show_data_messages.message_TotalSpent,
@@ -102,8 +118,9 @@ def get_show_data_text() -> Tuple[str]:
 
 def get_budget_system_text() -> Tuple[str]:
     """This gets the text for the budget_system"""
-
-    lang_budget_system_messages = Language.BudgetSystem
+    init_language_config()
+    
+    lang_budget_system_messages = Language.BudgetSystem()
 
     return (
         lang_budget_system_messages.message_FolderTreeDoesntExist,
@@ -111,8 +128,9 @@ def get_budget_system_text() -> Tuple[str]:
     
 def get_settings_text() -> Tuple[str]:
     """This gets the text for the settings"""
+    init_language_config()
 
-    lang_settings_messages = Language.Settings
+    lang_settings_messages = Language.Settings()
 
     return (
         lang_settings_messages.message_InvalidConfigFile,
